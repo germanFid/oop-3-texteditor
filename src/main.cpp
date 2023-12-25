98e90240395e8e241a7ba39bba8a7a20ed05bf73
germanFid <german.fidarov@gmail.com>
2023-12-25 03:25:29 +0300

    add basic implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
-#include "../MyString/src/mystring.h"
+#include "editor.h"
 
-int main()
-{
-    std::cout << "Hello!\n";
-    return 0;
+void run_editor(const char* file_name) {
+	Editor e{file_name};	
+
+	int character;
+	while (true) {
+		character = getch();
+		e.process_keypress(character);
+	}
+}
+
+int main(int argc, char** argv) {
+	if (argc > 0) {
+		run_editor(argv[1]);
+	}
 }
